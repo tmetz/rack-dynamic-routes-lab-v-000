97dc840be69a75f8d812c03d4a260e6f1ffe8e4a
Tammy Metz <tammy@tammymetz.com>
2018-10-07 16:25:13 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+ 
+  @@items = [Item.new("Bread", 2.99),
+            Item.new("Peanuts", 5.00)]
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last 
+      item = @@items.find{|i| i.name == item_name}
+      if item
+        resp.write item.price
+      else
+        resp.write "Sorry, item does not exist"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
+end
